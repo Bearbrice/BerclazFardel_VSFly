598db71c5a8bef9ecc9220c7bd13ff42dced793a
Bearbrice <berclazpro@gmail.com>
2020-05-20 09:14:29 +0200

    Implementing 2 solutions with architecture

---
+++ VSFly_BerclazFardel/EFCoreApp2020/Booking.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EFCoreApp2020
+{
+    public class Booking
+    {
+        // flight - passenger
+        public int FlightNo { get; set; }
+        public int PassengerID { get; set; }
+
+        public virtual Flight Flight { get; set;}
+        public virtual Passenger Passenger { get; set;}
+    }
+}
---
+++ VSFly_BerclazFardel/EFCoreApp2020/WWWingsContext.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+
+namespace EFCoreApp2020
+{
+    public class WWWingsContext : DbContext
+    {
+        public DbSet<Flight> FlightSet { get; set; }
+        public DbSet<Pilot> PilotSet { get; set; }
+
+        public DbSet<Passenger> PassengerSet { get; set; }
+        public DbSet<Booking> BookingSet { get; set; }
+
+        public static string ConnectionString { get; set; } = @"Server=(localDB)\MSSQLLocalDB;Database=WWWings_2020Corr1;Trusted_Connection=True;MultipleActiveResultSets=True;App=EFCoreApp2020";
+
+        public WWWingsContext() { }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder builder) {
+            builder.UseSqlServer(ConnectionString);
+
+            // convinient, flexible BUT DANGEROUS FOR PERFORMANCE
+            builder.UseLazyLoadingProxies();
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder) {
+
+            // composed
+            builder.Entity<Booking>().HasKey(x => new { x.FlightNo, x.PassengerID});
+
+            // mapping many to many relationship
+            builder.Entity<Booking>()
+                .HasOne(x => x.Flight)
+                .WithMany(x => x.BookingSet)
+                .HasForeignKey(x => x.FlightNo)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            builder.Entity<Booking>()
+                .HasOne(x => x.Passenger)
+                .WithMany(x => x.BookingSet)
+                .HasForeignKey(x => x.PassengerID)
+                .OnDelete(DeleteBehavior.Restrict);
+        }
+    }
+}
